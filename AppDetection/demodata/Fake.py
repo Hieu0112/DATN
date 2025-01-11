f093172c276512617538cc6c0e3301d5bf5f1b46
VietHieu2002 <trinhviethieu0112@gmail.com>
2025-01-12 00:36:13 +0700

    cập nhật demo thuyết trình

---
+++ AppDetection/demodata/Fake.py
@@ -0,0 +1,181 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+import csv
+import time
+import re
+from urllib.parse import urlparse
+
+# Cấu hình ChromeOptions
+chrome_options = Options()
+chrome_options.add_argument("--ignore-certificate-errors")  # Bỏ qua lỗi SSL
+
+# Khởi tạo trình điều khiển (ChromeDriver)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+
+# set_up_web="https://thoibao.de/blog/category/chinh-tri"
+# set_up_web="https://viettan.org/tai-sao-csvn-e-ngai-viet-tan/"
+# set_up_web="https://www.bbc.com/vietnamese/articles/c9vpj1gkmvko"
+set_up_web="https://viettan.org/nghi-dinh-168-nhung-he-qua-tai-hai/"
+
+# Mở một trang chứa reCAPTCHA để người dùng tự giải
+driver.get(set_up_web)
+input("Giải reCAPTCHA và nhấn Enter để tiếp tục...")  # Dừng lại cho đến khi reCAPTCHA được giải
+
+# Hàm làm sạch văn bản
+def clean_text(text):
+    text = re.sub(r'\s+', ' ', text)  # Loại bỏ khoảng trắng thừa
+    return text.strip() + " "
+
+# Hàm xử lý từng URL
+def process_url(url):
+    try:
+        driver.get(url)
+        time.sleep(1)  # Đợi trang tải
+        html_content = driver.page_source
+        soup = BeautifulSoup(html_content, 'html.parser')
+
+        parsed_url = urlparse(url)
+        domain = parsed_url.netloc
+        title_text = "N/A"
+        text_content = "N/A"
+        author = "N/A"
+        all_content = []
+
+        if "thoibao.de" in domain:
+            article = soup.find('article')
+            if article:
+                # Lấy Title từ thẻ <h1>
+                title = article.find('h1').get_text()
+                title_text = title.strip().replace('\n', ' ').replace('\t', ' ') if title else "N/A"
+                # Duyệt qua tất cả các thẻ <p> trong <article>
+                for p in article.find_all('p'):
+                    # Kiểm tra nếu thẻ <p> có <strong> thì là tác giả (Author)
+                    if p.find('strong'):
+                        author = p.get_text().strip().replace('\n', ' ').replace('\t', ' ')
+                    else:
+                        # Nếu không có <strong> thì lấy nội dung làm Text
+                        all_content.append(clean_text(p.get_text()))
+                full_content = ' '.join(all_content)
+                text_content = clean_text(full_content)
+        elif "bbc.com" in domain:
+            main_content = soup.find('main', class_='bbc-fa0wmp')
+
+            # Kiểm tra xem thẻ <main> có tồn tại
+            if main_content:
+                # Lấy tiêu đề từ thẻ <h1>
+                title = main_content.find('h1').get_text()
+                title_text = title.strip().replace('\n', ' ').replace('\t', ' ') if title else "N/A"
+                for p in main_content.find_all('p'):
+                    all_content.append(clean_text(p.get_text()))
+                
+                full_content = ' '.join(all_content)
+                text_content = clean_text(full_content)
+                
+                author_span = main_content.find('span', class_='bbc-18ttg5u')
+    
+                # Kiểm tra nếu thẻ <span> chứa tác giả tồn tại
+                if author_span:
+                    author = author_span.get_text().strip().replace('\n', ' ').replace('\t', ' ')
+        elif "khoi8406.com" in domain:
+            title = soup.find('h1')
+            title_text = title.text.strip().replace('\n', ' ').replace('\t', ' ') if title else "N/A"
+            
+            b_tags = soup.find_all('b')
+
+            for b_tag in b_tags:
+                if not b_tag.find('a'):  # Kiểm tra nếu không có thẻ <a> bên trong
+                    author= b_tag.get_text(strip=True).replace('\n', ' ').replace('\t', ' ')
+                    break
+            p_tags = soup.find_all('br')
+            for ct in p_tags:
+            # Sử dụng .next_sibling để truy cập nội dung sau thẻ <br>
+                next_sibling = ct.next_sibling
+
+                # Kiểm tra xem next_sibling có phải là một chuỗi văn bản không trống
+                if next_sibling and isinstance(next_sibling, str) and next_sibling.strip():
+                    all_content.append(clean_text(next_sibling.strip()))
+            # Join content into a single string
+            full_content = " ".join(all_content)
+            text_content = clean_text(full_content)
+        else:
+            # Lấy tiêu đề (thẻ h1)
+            title = soup.find('h1', class_='elementor-heading-title elementor-size-default')
+            title_text = title.text.strip().replace('\n', ' ').replace('\t', ' ') if title else "N/A"
+            # Lấy nội dung các thẻ <p> và tiêu đề <h>
+            content_block = soup.find('div', attrs={'class': ['elementor-widget-theme-post-content']})
+            if content_block:
+                for element in content_block.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p']):
+                    all_content.append(clean_text(element.get_text()))
+                full_content = ' '.join(all_content)
+                text_content = clean_text(full_content)
+            else:
+                text_content = "N/A"
+            # Lấy thông tin tác giả
+            author_info = soup.find('span', class_='elementor-icon-list-text elementor-post-info__item elementor-post-info__item--type-author')
+            author = author_info.text.strip() if author_info else "N/A"
+        
+
+
+        # Trả về dữ liệu đã lấy
+        return {
+            'title': title_text,
+            'text': text_content.replace('\n', ' '),
+        }
+    except Exception as e:
+        print(f'Yêu cầu thất bại với URL {url}. Lỗi: {e}')
+        return None
+
+# Đường dẫn đến file chứa URL và file lưu dữ liệu đã xử lý
+urls_file = 'AppDetection/demodata/URLsFake.csv'
+
+fake_file = 'AppDetection/demodata/FakeDetail.csv'
+
+# Đọc URL từ file CSV và ghi dữ liệu đã xử lý
+with open(urls_file, mode='r', newline='', encoding='utf-8') as url_file:
+    reader = csv.DictReader(url_file)
+    rows = list(reader)  # Đọc tất cả các dòng vào danh sách
+maxs_rows = min(500,len(rows))
+i=1
+# Lặp qua từng URL trong file CSV
+for row in rows:
+    if i <= maxs_rows:
+        url = row['link']  # Giả sử file CSV có cột 'link' chứa các URL
+        processed = row.get('Processed', '0')  # Kiểm tra xem URL đã được xử lý chưa
+
+        if processed == '0':  # Chỉ xử lý các URL chưa được xử lý
+            print(f"Đang xử lý: {url}")
+            i+=1
+            # Xử lý từng URL
+            article_data = process_url(url)
+            
+            if article_data:
+                # Ghi dữ liệu vào file CSV
+                with open(fake_file, mode='a', newline='', encoding='utf-8') as file:
+                    writer = csv.DictWriter(file, fieldnames=['title', 'text']) 
+                    
+                    # Nếu file trống, ghi tiêu đề vào
+                    if file.tell() == 0:
+                        writer.writeheader()
+                    
+                    # Ghi dữ liệu bài báo nếu 'text' không rỗng
+                    if article_data['text'].strip():
+                        writer.writerow(article_data)
+                    else:
+                        print(f"Không có nội dung hợp lệ tại {url}")
+
+                # Sau khi xử lý xong, đánh dấu 'Processed' là 1
+                row['Processed'] = '1'
+
+            time.sleep(0.3)  # Đợi một chút giữa các yêu cầu
+
+# Ghi lại file CSV với cột 'Processed' đã được cập nhật
+with open(urls_file, mode='w', newline='', encoding='utf-8') as url_file:
+    writer = csv.DictWriter(url_file, fieldnames=reader.fieldnames)
+    writer.writeheader()  # Ghi tiêu đề
+    writer.writerows(rows)  # Ghi lại toàn bộ các hàng, bao gồm cả cột 'Processed' đã cập nhật
+
+print(f"Dữ liệu đã được lưu vào {fake_file}")
+driver.quit()
---
+++ AppDetection/demodata/Real.py
@@ -0,0 +1,181 @@
+import csv
+import os
+import time
+import re
+from bs4 import BeautifulSoup
+import requests
+from urllib.parse import urlparse
+
+# Đường dẫn tới tệp CSV chứa URL
+urls_file = 'AppDetection/demodata/URLsReal.csv'
+
+real_file = 'AppDetection/demodata/RealDetail.csv'
+
+def clean_text(text):
+    # Thay thế các ký tự xuống dòng và nhiều dấu cách bằng một dấu cách
+    text = re.sub(r'\s+', ' ', text)
+    return text.strip()+" "
+
+def removeDate_author(text):
+    # Định nghĩa mẫu cho định dạng ngày giờ
+    pattern = r'\b\d{2}/\d{2}/\d{4} - \d{2}:\d{2}\b'
+    
+    # Kiểm tra nếu chuỗi chứa định dạng ngày giờ
+    if re.search(pattern, text):
+        return "N/A"
+    else:
+        return text
+
+def process_url(url):
+    try:
+        print(f"Đang xử lý: {url}")
+        # Gửi yêu cầu HTTP đến URL và lấy nội dung
+        response = requests.get(url)
+        # response = requests.get(url, timeout=30)  # Thời gian chờ tối đa là 30 giây
+        response.raise_for_status()  # Kiểm tra trạng thái yêu cầu HTTP
+        html_content = response.text
+        # Phân tích nội dung HTML bằng BeautifulSoup
+        soup = BeautifulSoup(html_content, 'html.parser')
+        # Tìm thẻ <h1>
+        h1_tag = soup.find('h1')
+
+        # Trích xuất domain từ URL
+        parsed_url = urlparse(url)
+        domain = parsed_url.netloc
+
+        # Tìm thẻ tác giả
+        author_content = 'N/A'
+            
+        # Kiểm tra các thẻ <p> để tìm thông tin tác giả
+        # Tìm thẻ có lớp chứa chữ 'author'
+        author_paragraph = soup.find('p', class_=lambda x: x and 'author' in x)
+
+        # Kiểm tra nếu thẻ author_paragraph không phải là None
+        if author_paragraph:
+            text = author_paragraph.get_text(strip=True)
+            if len(text) > 0:
+            # Xóa ký tự \n và thay thế \t bằng dấu cách
+                author_content = text.replace('\n', ' ').replace('\t', ' ')
+
+        # Nếu không tìm thấy trong thẻ <p>, kiểm tra thẻ có class chứa "author"
+        if author_content == 'N/A':
+            author_tag = soup.find(class_=lambda class_name: class_name and 'author' in class_name)
+            if author_tag:
+                a_tags = author_tag.find_all('a')
+                for a_tag in a_tags:
+                    if a_tag.get_text(strip=True):  # Kiểm tra nội dung không trống
+                        author_content = a_tag.get_text(strip=True)
+                        break
+
+        if author_content == 'N/A':
+            all_paragraphs = soup.find_all('p')
+
+            # Chọn các thẻ <p> từ vị trí thứ 5 từ dưới lên
+            if len(all_paragraphs) > 2:
+                paragraphs_to_check = all_paragraphs[-2:]  # 5 thẻ cuối cùng
+            else:
+                paragraphs_to_check = all_paragraphs  # Nếu ít hơn 5 thẻ, lấy tất cả
+            for p in paragraphs_to_check:
+                if p.find('strong'):
+                    text = p.get_text(strip=True)
+                    if len(text) > 0:
+                        # Xóa ký tự \n và thay thế \t bằng dấu cách
+                        author_content = text.replace('\n', ' ').replace('\t', ' ')
+        if author_content == 'N/A':
+            author_paragraph = soup.find('div', class_=lambda x: x and 'author' in x)
+            # Kiểm tra nếu thẻ author_paragraph không phải là None
+            if author_paragraph:
+                text = author_paragraph.get_text(strip=True)
+                if len(text) > 0:
+                # Xóa ký tự \n và thay thế \t bằng dấu cách
+                    author_content = text.replace('\n', ' ').replace('\t', ' ')  
+        if h1_tag:
+            # Lấy nội dung của thẻ <h1>
+            h1_text = h1_tag.get_text().strip()
+
+            # Tìm thẻ cha của thẻ <h1>
+            parent_tag = h1_tag.find_parent()
+
+            if parent_tag:
+                # Tìm tất cả các thẻ <p> bên trong thẻ cha, ngoại trừ các thẻ nằm trong thẻ author
+                p_tags = parent_tag.find_all('p')
+                p_texts = []
+                for p_tag in p_tags:
+                    # Kiểm tra nếu thẻ <p> không nằm trong thẻ có class chứa từ "author"
+                    parent_author_tag = p_tag.find_parent(class_=lambda class_name: class_name and 'author' in class_name)
+                    if not parent_author_tag:  # Nếu không tìm thấy thẻ cha với class chứa "author"
+                        p_content = clean_text(p_tag.get_text().strip())
+                        p_texts.append(p_content)
+
+                # Nối nội dung các thẻ <p> bằng dấu chấm
+                p_content = '. '.join(p_texts)
+                #  đời sống pháp luật
+                if len(p_content)==0:
+                    p_tags = soup.find_all('p')
+                    for p_tag in p_tags:
+                        # Kiểm tra nếu thẻ <p> không có class chứa từ "author"
+                        if not p_tag.has_attr('class') or not any('author' in class_name for class_name in p_tag['class']):
+                            # Lấy và xử lý nội dung của thẻ <p>
+                            p_content = clean_text(p_tag.get_text().strip())
+                            p_texts.append(p_content)
+                            doisongphapluat_web = 'uppercase bold color-blue-1'
+                            tag = soup.find(class_=doisongphapluat_web)
+                            if tag:
+                                author_content = tag.get_text(strip=True)
+                    # Sau khi lặp qua tất cả các thẻ <p>, nối nội dung các thẻ thành một chuỗi
+                    p_content = '. '.join(p_texts)
+
+            return{
+                'title': h1_text,
+                'text': p_content.replace('\n', ' ')
+            }
+    except requests.exceptions.Timeout:
+        print(f"Request to {url} timed out.")
+    except requests.RequestException as e:
+        print(f'Yêu cầu thất bại với URL {url}. Lỗi: {e}')
+        return None
+
+
+# Kiểm tra sự tồn tại của tệp CSV
+data = []
+i=1
+with open(urls_file, mode='r', newline='', encoding='utf-8') as file:
+    reader = csv.DictReader(file)
+    rows = list(reader)
+    maxs_rows=min(1500,len(rows))
+    for row in rows:
+        if i<= maxs_rows:
+            if row['Processed'] == '0':  # Chỉ xử lý khi 'Processed' là 0
+                article_data = process_url(row['link'])  # Gọi hàm để xử lý bài báo
+                i+=1
+                # Ghi dữ liệu vào file real.csv
+                if article_data:
+                    with open(real_file, mode='a', newline='', encoding='utf-8') as file:
+                        writer = csv.DictWriter(file, fieldnames=['title', 'text'])
+                    
+                        # Nếu file trống thì ghi tiêu đề vào
+                        if file.tell() == 0:
+                            writer.writeheader()
+                    
+                        # Ghi dữ liệu bài báo
+                        if article_data['text'].strip():  # Kiểm tra nếu 'text' không rỗng
+                            # Ghi dữ liệu bài báo
+                            writer.writerow(article_data)
+                            # Sau khi xử lý xong, đánh dấu 'Processed' là 1
+                            row['Processed'] = '1'
+                            print("Dữ liệu số: "+str(i))
+                    time.sleep(0.3)
+                else:
+                    print('x'+str(i))
+        data.append(row)  # Lưu lại dữ liệu
+
+with open(urls_file, mode='w', newline='', encoding='utf-8') as file:
+    writer = csv.DictWriter(file, fieldnames=['title', 'link', 'Processed'])
+    
+    if file.tell() == 0:
+        writer.writeheader()
+    
+    # Ghi các dòng dữ liệu đã được cập nhật
+    writer.writerows(data)
+
+print("Dữ liệu đã được xử lý và cập nhật.")
